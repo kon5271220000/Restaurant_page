c15f3ed0be0807d388d4efab779fc85ec3ca3bcd
kon5271220000 <kon527122000@gmail.com>
2025-01-23 16:20:00 +0700

    add about.js, finish basic js event, basic html

---
+++ src/about.js
@@ -0,0 +1,35 @@
+
+export const loadAboutPage = () => {
+    const container = document.getElementById("content")
+
+    addInfo("Address: ", "320, not real str", container)
+    addInfo("Phone: ", "555-420-555", container)
+    addInfo("Email: ", "notRealEmail@gmail.com", container)
+    addInfo("Website: ", "www.notRealWebstite.com", container)
+    addOpenInfo(container)
+}
+
+function addInfo(heading, content, holder){
+    const div = document.createElement("div")
+    const strong = document.createElement("strong")
+    const p = document.createElement("p")
+    strong.textContent = heading
+    p.textContent = content
+    div.appendChild(strong)
+    div.appendChild(p)
+    holder.appendChild(div)
+}
+
+function addOpenInfo(container) {
+    const div = document.createElement("div")
+    const strong = document.createElement("strong")
+    const p1 = document.createElement("p")
+    const p2 = document.createElement("p")
+    strong.textContent = "Opening Hours:"
+    p1.textContent = "Monday to Friday: 8am-10pm"
+    p2.textContent = "Saturday and Sunday: 8am-2pm"
+    div.appendChild(strong)
+    div.appendChild(p1)
+    div.appendChild(p2)
+    container.appendChild(div)
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import { loadHomePage } from "./home.js";
 import { loadMenuPage } from "./menu.js";
+import { loadAboutPage } from "./about.js";
 
 loadHomePage()
 
 const mainContent = document.getElementById("content")
 
+document.getElementById("home").addEventListener('click', () => {
+    mainContent.innerHTML = ''
+    loadHomePage()
+})
+
 document.getElementById("menu").addEventListener("click", () => {
     mainContent.innerHTML = ''
     loadMenuPage()
 })
+
+document.getElementById("about").addEventListener('click', () => {
+    mainContent.innerHTML = ''
+    loadAboutPage()
+})
+
